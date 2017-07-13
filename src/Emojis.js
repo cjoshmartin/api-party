32bb81ff18c49f95013fb8ce5e81d564bbf0d21a
Josh Martin <joshmartin5001@gmail.com>
2017-07-13 01:35:37 -0400

    cleanning

--- src/Emojis.js
+++ src/Emojis.js
@@ -3,17 +3,30 @@ import { Route, NavLink } from 'react-router-dom'
 
 
 export default class Emojis extends Component {
-    state ={
-        emoji:{}
+  constructor(props){
+    super(props)
+    this.state = {
+      emojis:{},
+      currenttext :"",
+      currentImg: "",
     }
-    render = () => {
+
+    fetch('https://api.github.com/emojis').then((response) => response.json()).then((r) => {
+      const keys = Object.keys(r)
+      const randomNum = Math.floor(Math.random() * keys.length)
+      this.setState({emojis:r, currenttext: keys[randomNum],currentImg:r[keys[randomNum]]})
+      })
+  }
+    render() {
         return (
             <div>
-                <h1>Random</h1>
+                {this.state.currenttext ? <h1 style={{fontSize:"2.8rem"}}>{this.state.currenttext.split('_').join(' ')}</h1> : <h1 style={{fontSize:"2.8rem"}}>Random</h1>}
+                {this.state.currentImg ? <img src={this.state.currentImg} /> : ""}
+                <br />
                 <button>Push Me to Get a Random Emoji</button>
+                {console.log(this.state)}
             </div>
         )
     }
 
 }
-
